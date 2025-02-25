6cc5f194d2daa862ac547d79142e4c746e63ad98
snaramirez872 <seanramirez818@gmail.com>
2025-02-25 17:39:30 -0500

    ready for api implementation

--- components/pokemonCards.tsx
+++ components/pokemonCards.tsx
@@ -8,10 +8,12 @@ export function PokemonCard({ name } : PokemonCardProps) {
     return (
         <Link
             href={name.toLowerCase()}
-            className="group rounded-lg border border-transparent px-8 py-4 text-center w-fit transition-colors hover:border-red-300"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group rounded-lg border border-transparent m-3 px-5 py-4 text-center w-fit transition-colors dark:border-red-300"
             key={name + "Card"}
         >
-            <h2 className="mb-3 text-2xl font-semibold">
+            <h2 className="text-2xl font-semibold">
                 {name}
             </h2>
         </Link>
--- components/pokemonGrid.tsx
+++ components/pokemonGrid.tsx
@@ -6,23 +6,106 @@ export function PokemonGrid() {
     const [ searchText, setSearchText ] = useState("");
 
     return (
-        <>
+        <div className="flex flex-col justify-center">
             <div>
-                <h3 className="text-2xl py-6 text-center">Search for Your Pokemon!</h3>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
+                <h3 className="text-2xl py-6 text-center">Pokemon Type Effectiveness</h3>
+                <div className="flex flex-col justify-center items-center">
                     <label htmlFor='pkmn-name'>Pokemon Name</label>
                     <input 
                         type='text' 
-                        id="pkmn-name" 
+                        id="pkmn-name"
+                        autoCorrect="off"
+                        className="text-left px-2 py-1 w-13 text-black-500"
                         placeholder='Charizard, Venusaur, Blastoise, Pikachu, etc.' 
                         value={searchText}
                         onChange={(ev) => setSearchText(ev.target.value)}
                     />
                 </div>
+                {searchText && (
+                    <div className="text-center py-2">
+                        <h4>You Entered: {searchText.toUpperCase()}</h4>
+                    </div>
+                )}
             </div>
-            <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <PokemonCard name="Pikachu" />
+            <div id="container" className="overflow-y-scroll h-[50vh] w-[120vh] mx-auto">
+                <div className="mb-32 grid place-items-center text-center grid-cols-4 overflow-hidden">
+                    <PokemonCard name="Venusaur" />
+                    <PokemonCard name="Blastoise" />
+                    <PokemonCard name="Charizard" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                    <PokemonCard name="Pikachu" />
+                </div>
             </div>
-        </>
+        </div>
     );
 }
