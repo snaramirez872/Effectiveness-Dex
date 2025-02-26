fe925743ebccf2b3a6d134b4e99ecf1b2b2a5791
snaramirez872 <seanramirez818@gmail.com>
2025-02-26 13:30:08 -0500

    last commit was a fluke, made more fixes to make ESLint happy

--- lib/pokemonAPI.ts
+++ lib/pokemonAPI.ts
@@ -1,23 +1,32 @@
+interface PokemonListItem {
+    name: string;
+    url: string;
+}
+
 interface PokemonDetails {
     name: string;
     url: string;
 }
 
-interface RESPONSE {
+interface PKMN_LIST_RESPONSE {
+    results: PokemonDetails[];
+}
+
+interface DETAILS_RESPONSE {
     results: PokemonDetails[];
 }
 
 // Getting Pokemon List
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export async function getPokemonList(): Promise<PokemonDetails[]> {
+export async function getPokemonList(): Promise<PokemonListItem[]> {
     const resp = await fetch(BASE_URL + "pokemon?limit=100000&offset=0");
-    const dat = await resp.json();
+    const dat: PKMN_LIST_RESPONSE = await resp.json();
     return dat.results;
 }
 
 // Getting desired pokemon's details
 export async function getPKMNDetails(name : string): Promise<PokemonDetails[]> {
     const resp = await fetch(BASE_URL + `pokemon/${name}`);
-    const dat = await resp.json();
+    const dat: DETAILS_RESPONSE = await resp.json();
     return dat.results;
 } 
